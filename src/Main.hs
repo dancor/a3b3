d53863ed8eaa25ce596d82ac5f6324f81de9f0a9
Daniel Corson <danl@alum.mit.edu>
2010-04-21 19:13:07 -0700

    findCDs (slow, 240s on 10k data)

--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,38 @@
 module Main where
 
+import Control.Applicative
 import Control.Monad
 import Data.Maybe
+import Data.Monoid
 import System.Environment
 import qualified Data.IntMap as IM
-import qualified Data.IntSet as IS
 import qualified Foreign.C.Math.Double as F
 
 -- will i ever be fast enough to need Integer even on 64bit?  prob not
 type I = Int
+type R = (I, I, I, I)
 
-cbrtCeil = ceiling . F.cbrt . fromIntegral
+cbrt = round . F.cbrt . fromIntegral
 
-aUpTo :: I -> [(I, I, I)]
-aUpTo n = do
-  a <- [1..n]
-  b <- [1..a]
+findCDs :: (I, I) -> [R]
+findCDs (a, b) = do
   let s = cube a + cube b
-  -- doing upper half (c from ceil) here is faster than lower half (d to floor)
-  c <- [cbrtCeil $ (s + 1) `div` 2 .. a - 1]
-  guard $ IS.member (s - cube c) cubeS
-  return (a, b, c)
+  c <- [cbrt $ (s + 1) `div` 2 .. a - 1]
+  let d3 = s - cube c
+  case IM.lookup d3 cubeRev of
+    Just d -> return (a, b, c, d)
+    _ -> mempty
   where
-  cubeL = map (^3) [1..n]
-  cubeS = IS.fromList cubeL
-  cubeM = IM.fromDistinctAscList $ zip [1..n] cubeL
+  cubeL = map (^3) [1..a]
+  cubeM = IM.fromDistinctAscList $ zip [1..a] cubeL
+  cubeRev = IM.fromDistinctAscList $ zip cubeL [1..a]
   cube = fromJust . flip IM.lookup cubeM
 
 main :: IO ()
 main = do
-  [n] <- getArgs
-  putStr . unlines . map show . aUpTo $ read n
+  [f] <- getArgs
+  aBs <- map ((\ [a, b] -> (read a, read b)) . words) . lines <$> readFile f
+  putStr . unlines . map myShow $ concatMap findCDs aBs
+
+myShow :: R -> String
+myShow (a, b, c, d) = show a ++ " " ++ show b ++ " " ++ show c ++ " " ++ show d
